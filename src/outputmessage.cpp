fabdc0440e86632e5bbf9a59ae75b3974dfa8c35
dr90-del <danielrutty90@gmail.com>
2025-06-13 10:14:34 +0100

    Update outputmessage.cpp

--- src/outputmessage.cpp
+++ src/outputmessage.cpp
@@ -31,53 +31,59 @@ const std::chrono::milliseconds OUTPUTMESSAGE_AUTOSEND_DELAY {10};
 
 class OutputMessageAllocator
 {
-	public:
-		typedef OutputMessage value_type;
-		template<typename U>
-		struct rebind {typedef LockfreePoolingAllocator<U, OUTPUTMESSAGE_FREE_LIST_CAPACITY> other;};
+    public:
+        typedef OutputMessage value_type;
+
+        // <<< patched for GCC 13 allocator_traits compatibility >>>
+        // Now, rebind<any U> gives back this same allocator type.
+        template<typename U>
+        struct rebind {
+            using other = OutputMessageAllocator;
+        };
+        // <<< end patch >>>
 };
 
 void OutputMessagePool::scheduleSendAll()
 {
-	auto functor = std::bind(&OutputMessagePool::sendAll, this);
-	g_scheduler.addEvent(createSchedulerTask(OUTPUTMESSAGE_AUTOSEND_DELAY.count(), functor));
+    auto functor = std::bind(&OutputMessagePool::sendAll, this);
+    g_scheduler.addEvent(createSchedulerTask(OUTPUTMESSAGE_AUTOSEND_DELAY.count(), functor));
 }
 
 void OutputMessagePool::sendAll()
 {
-	//dispatcher thread
-	for (auto& protocol : bufferedProtocols) {
-		auto& msg = protocol->getCurrentBuffer();
-		if (msg) {
-			protocol->send(std::move(msg));
-		}
-	}
+    //dispatcher thread
+    for (auto& protocol : bufferedProtocols) {
+        auto& msg = protocol->getCurrentBuffer();
+        if (msg) {
+            protocol->send(std::move(msg));
+        }
+    }
 
-	if (!bufferedProtocols.empty()) {
-		scheduleSendAll();
-	}
+    if (!bufferedProtocols.empty()) {
+        scheduleSendAll();
+    }
 }
 
 void OutputMessagePool::addProtocolToAutosend(Protocol_ptr protocol)
 {
-	//dispatcher thread
-	if (bufferedProtocols.empty()) {
-		scheduleSendAll();
-	}
-	bufferedProtocols.emplace_back(protocol);
+    //dispatcher thread
+    if (bufferedProtocols.empty()) {
+        scheduleSendAll();
+    }
+    bufferedProtocols.emplace_back(protocol);
 }
 
 void OutputMessagePool::removeProtocolFromAutosend(const Protocol_ptr& protocol)
 {
-	//dispatcher thread
-	auto it = std::find(bufferedProtocols.begin(), bufferedProtocols.end(), protocol);
-	if (it != bufferedProtocols.end()) {
-		std::swap(*it, bufferedProtocols.back());
-		bufferedProtocols.pop_back();
-	}
+    //dispatcher thread
+    auto it = std::find(bufferedProtocols.begin(), bufferedProtocols.end(), protocol);
+    if (it != bufferedProtocols.end()) {
+        std::swap(*it, bufferedProtocols.back());
+        bufferedProtocols.pop_back();
+    }
 }
 
 OutputMessage_ptr OutputMessagePool::getOutputMessage()
 {
-	return std::allocate_shared<OutputMessage>(OutputMessageAllocator());
+    return std::allocate_shared<OutputMessage>(OutputMessageAllocator());
 }
